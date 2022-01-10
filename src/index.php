4d78c55cdd58b61199ea86c66473cc15d33d7d22
Clément LEFIEF <clement.lefief@gmail.com>
2022-01-10 15:50:56 +0100

    change date

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 2 déc. 2021 à 16h31 </p>';
+echo '<p>update : 10 Janvier 2022 à 15h50 </p>';
 echo '<p>bonjour je suis tatadozz</p>';
 phpinfo();
 ?>
