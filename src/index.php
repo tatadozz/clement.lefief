d1007f75a047fe56abe4a5676cff58f16abb1b7b
Clément LEFIEF <clement.lefief@gmail.com>
2022-01-10 15:06:15 +0100

    add text

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 2 déc. 2021 à 16h31 </p>';
+echo '<p>bonjour je suis tatadozz</p>';
 phpinfo();
 ?>
 
